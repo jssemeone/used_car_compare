3880d57b0815cb9b76496b04cb1701587057fe9f
Tatyana <twinki2525@mail.ru>
2019-06-22 14:11:28 +0300

    выбрала характеристики авто

---
+++ webapp/models.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_html(url):
+    try:
+        result = requests.get(url)
+        result.raise_for_status()
+        return result.text
+    except(requests.RequestException, ValueError):
+        print('Сетевая ошибка')
+        return False
+
+def get_auto_specifications(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    all_specific = soup.find(class_="b-model-specs").findAll(class_="bm-modelSpecsGroup")
+    print(all_specific)
+
+if __name__ == "__main__":
+        html = get_html("https://www.drom.ru/catalog/ford/focus/156523/")
+        if html:
+            get_auto_specifications(html)
+

73eb9d8b08ec551f4c15a27d7215f0b9c0ea89eb
jssemeone <jssemeone@mail.ru>
2019-06-29 12:21:23 +0300

    Внес изменения в соответствии с лекцией, все равно не робит

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,13 +1,14 @@
+from flask import current_app
 from flask import Flask, render_template
-
-
-app = Flask(__name__)
 from webapp.news_gibdd import get_gibdd_news
 
-@app.route("/")
-def main():
-    news_list = get_gibdd_news
-    return render_template('index.html', news_list = news_list)
 
-if __name__ == "__main__":
-    app.run()
+def create_app():
+    app = Flask(__name__)
+
+    @app.route("/")
+    def main():
+        news_list = get_gibdd_news
+        return render_template('index.html', news_list = news_list)
+
+    return app

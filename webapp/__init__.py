9f9b85ba89edc566d3363a34079b3fcdecb76f47
jssemeone <jssemeone@mail.ru>
2019-07-10 23:21:00 +0300

    добавил привязку бд к приложению, подправил футер, создал main_css

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -5,10 +5,11 @@ from flask import Flask, render_template
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile("config.py")
+    db.init_app(app)
 
     @app.route("/")
     def main():
-        news_list = News.query.all()
+        news_list = News.query.limit(4).all()
         return render_template('index.html', news_list = news_list)
 
     return app

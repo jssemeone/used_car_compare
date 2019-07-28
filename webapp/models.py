d45c1b306c20f73acd1319d67f18203199f22c38
Tatyana <twinki2525@mail.ru>
2019-07-28 20:30:23 +0300

    first try to rewrite parser

--- webapp/models.py
+++ webapp/models.py
@@ -1,6 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+list_of_interest = [
+'Название марки',
+'Название модели',
+'Название комплектации',
+'Период выпуска',
+'Тип привода',
+'Тип кузова',
+'Тип трансмиссии',
+'Объем двигателя, куб.см',
+'Страна сборки',
+'Марка двигателя',
+'Максимальная мощность, л.с. (кВт) при об./мин.',
+'Максимальный крутящий момент, Н*м (кг*м) при об./мин.',
+'Тип двигателя',
+'Используемое топливо',
+'Расход топлива в городском цикле, л/100 км',
+'Расход топлива за городом, л/100 км',
+'Расход топлива в смешанном цикле, л/100 км',
+'Передняя подвеска',
+'Задняя подвеска',
+# 'Шины',
+'Передние колеса',
+'Задние колеса',
+# 'Микроклимат салона',
+'Кондиционер',
+# 'Салонный фильтр',
+# 'Климат-контроль',
+# 'Средняя стоимость'
+]
+
+
 def get_html(url):
     try:
         result = requests.get(url)
@@ -10,40 +42,36 @@ def get_html(url):
         print('Сетевая ошибка')
         return False
 
+
 def get_auto_specifications(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    all_specific = soup.findAll(class_="b-model-specs__label")
 
     characteristics = {}
     
-    for tag in soup.find_all(class_="bm-modelSpecsGroup"):
-        label = tag.find(class_="b-model-specs__label").text
-        value = tag.find(class_="b-model-specs__text").text.strip()
-        characteristics[label] = value
+    soup = BeautifulSoup(html, 'html.parser')
+    path_line = soup.find_all(class_="b-breadcrumbs__item")
+
+    characteristics['Название марки'] = path_line[2].text
+    characteristics['Название модели'] = path_line[3].text
+
+    table1 = soup.find_all(class_='b-table b-table_mobile-size-s b-table_text-size-s b-table_text-left')[0]
+
+    row_class_ = 'b-table__row b-table__row_padding_l-r-size-xs '+\
+               'b-table__row_cols_2 b-table__row_border_bottom b-table__row_border_light '+\
+               'b-table__row_padding_t-b-size-s b-table_align_top'
+
+    for row in table1.find_all('tr', class_=row_class_):
+        key = row.find_all('td')[0].text
+        value = row.find_all('td')[1].text
+        if key in ['Передние колеса','Задние колеса']:
+            value = value.split('Отзывы')[0]
+        characteristics[key] = value.strip('\n ')
     
-    # print(characteristics['Объем'])
-    return characteristics
+    return {key:value for key, value in characteristics.items() if key in list_of_interest}
 
-
-def process_raw_characteristics(raw_characteristics):
-    raw_characteristics =   {'Передние шины': '195/65 R15', 'Задние шины': '195/65 R15', 'Объем': '1.4 л', 'Мощность': '80 л.с.', 'Расход': '6,6 л', 'Тип топлива': 'Бензин', 'Трансмиссия': 'МКПП', 'Привод': 'Передний (FF)', 'Кол-во мест': '5 мест', 'Кол-во дверей': '3 двери', 'Клиренс': '155 мм', 'Объем багажника': '385 л'}    
-    
-    processed_characteristics = {}
-
-    for charact in raw_characteristics:
-        raw_characteristics['volume'] = raw_characteristics.pop('Объем')
-        raw_characteristics['power'] = raw_characteristics.pop('Мощность')
-        raw_characteristics['consumption'] = raw_characteristics.pop('Расход')
-        print(raw_characteristics)
-
-        
-    # processed_characteristics = {'volume': 1.4, 'power': 6.6}
-    # return processed_characteristics
 
 if __name__ == "__main__":
+
     html = get_html("https://www.drom.ru/catalog/ford/focus/156523/")
+
     if html:
-        get_auto_specifications(html)
-
-
-# dictionary[new_key] = dictionary.pop(old_key)
+        characteristics = get_auto_specifications(html)

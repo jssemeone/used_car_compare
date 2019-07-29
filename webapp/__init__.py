d8684221f9aa50beab0b70ef671ab02c40286698
jssemeone <jssemeone@mail.ru>
2019-07-29 07:44:14 +0300

    Добавил форму логин (проблема с импортом), изменил requarement

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,5 +1,6 @@
 from webapp.model import db, News
 from flask import Flask, render_template
+from webapp.forms import LoginForm
 
 
 def create_app():
@@ -11,5 +12,11 @@ def create_app():
     def main():
         news_list = News.query.limit(5).all()
         return render_template('index.html', news_list = news_list)
+
+    @app.route('/login')
+    def login():
+        title = "Авторизация"
+        login_form = LoginForm()
+        return render_template('login.html', page_title=title, form=login_form)
 
     return app
--- webapp/config.py
+++ webapp/config.py
@@ -3,3 +3,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 NEWS_URL = "https://xn--90adear.xn--p1ai/news/federal"
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
+SECRET_KEY = "rfglwerba;ub37AADIhuhhh1122++/=TTj;eevnj"

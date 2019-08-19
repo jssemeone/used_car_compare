d99f898eca746e1f12e511553ce0eb8f6255f558
jssemeone <jssemeone@mail.ru>
2019-08-19 07:35:50 +0300

    Добавил блюпринты news, admin при переименовании model.py в db.py вылезла проблема с импортами

--- webapp/news/models.py
+++ webapp/news/models.py
@@ -1,6 +1,5 @@
-from flask_sqlalchemy import SQLAlchemy
+from webapp.db import db
 
-db = SQLAlchemy()
 
 class News(db.Model):
     id = db.Column(db.Integer, primary_key=True)
---
+++ webapp/news/views.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+
+from webapp.news.models import News
+
+blueprint = Blueprint('news', __name__)
+
+
+@blueprint.route("/")
+def main():
+    news_list = News.query.limit(5).all()
+    return render_template('index.html', news_list = news_list)
